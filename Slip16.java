7084254443350920949ae2f93e32560a3f95da6b
hptryk <147969227+hptryk@users.noreply.github.com>
2024-04-17 23:06:22 +0530

    Slip16.java

--- Slip16.java
+++ Slip16.java
@@ -29,57 +29,42 @@ public class Slip16
 
 /*
 import java.sql.*;
-import java.io.*;
-class Slip16
+public class TeacherDetails 
 {
-    public static void main(String a[])
+    public static void main(String[] args) 
     {
-        PreparedStatement ps;
-        Connection con;
-        try
+        try 
         {
-          Class.forName("org.postgresql.Driver");
-          con=DriverManager.getConnection("jdbc:postgresql://192.168.1.254/ty32","ty32","ty32");
-          if(con==null)
-          {
-            System.out.println("Connection Failed......");
-            System.exit(1);
-          }
-          System.out.println("Connection Esatablished......");
-          Statement stmt=con.createStatement();
-          BufferedReader br = new BufferedReader(new 
-          InputStreamReader(System.in));
-          String query="insert into Customer values(?,?,?,?)";
-          ps=con.prepareStatement(query);
-          
-          System.out.println("Customer Details....");
-          System.out.println("Enter CID");
-          int cid=Integer.parseInt(br.readLine()); 
-          ps.setInt(1,cid); 
+            Connection connection =  DriverManager.getConnection("jdbc:postgresql://192.168.1.254/ty32","ty32","t32"");
+            displayJavaTeachers(connection);
 
-          System.out.println("Enter name");
-          String name=br.readLine();
-          ps.setString(2,name);
-
-          System.out.println("Enter Address");
-          String add=br.readLine();
-          ps.setString(3,add);
-          
-          System.out.println("Enter Ph_No");
-          String phno=br.readLine();
-          ps.setString(4,phno);
-          int no=ps.executeUpdate();
-          if(no!=0)
-          
-          System.out.println("Data inserted succesfully.....");
-          else
-          System.out.println("Data NOT inserted");
-          con.close();
-        }
-        catch(Exception e)
+            connection.close();
+        } 
+        catch (SQLException e) 
         {
             e.printStackTrace();
         }
-      }
+    }   
+    private static void displayJavaTeachers(Connection connection) throws SQLException 
+    {
+        String selectQuery = "SELECT * FROM Teacher WHERE Subject = ?";
+
+        PreparedStatement preparedStatement = connection.prepareStatement(selectQuery);
+
+        preparedStatement.setString(1, "JAVA");
+        ResultSet resultSet = preparedStatement.executeQuery();
+
+        // Display details of Java teachers
+        System.out.println("Details of teachers teaching JAVA subject:");
+        System.out.println("-------------------------------------------");
+        while (resultSet.next()) 
+        {
+            int tNo = resultSet.getInt("TNo");
+            String tName = resultSet.getString("TName");
+            String subject = resultSet.getString("Subject");
+            System.out.println("TNo: " + tNo + ", TName: " + tName + ", Subject: " + subject);
+        }
+    }
 }
 */
+
